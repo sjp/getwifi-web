b2af808bd9321e8c9e4d0a6aa1b3480690fd92e1
Simon Potter <simon@sjp.co.nz>
2023-03-26 18:54:46 +1300

    Add routing and fix CI

---
+++ src/root.tsx
@@ -0,0 +1,29 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./root.css";
+
+export const Root = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank" rel="noreferrer">
+          <img src={preactLogo} className="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/root.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and Preact logos to learn more</p>
+    </>
+  );
+};
