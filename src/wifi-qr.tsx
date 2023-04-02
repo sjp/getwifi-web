2f93748e284a120b4b854c42e9b3379a4f5a51a8
Simon Potter <simon@sjp.co.nz>
2023-04-02 21:42:22 +1200

    Add basic offline support (messaging only).

--- src/wifi-qr.tsx
+++ src/wifi-qr.tsx
@@ -1,3 +1,4 @@
+import { QRCodeSVG } from "qrcode.react";
 import { WifiDetails } from "./models";
 
 const escape = (input: string): string => {
@@ -13,10 +14,30 @@ const escape = (input: string): string => {
     .replaceAll(":", "\\:");
 };
 
-export const generateQrCode = (input: WifiDetails): string => {
+const generateQrCode = (input: WifiDetails): string => {
   const ssid = `S:${escape(input.ssid)};`;
   const password = input.password ? `P:${escape(input.password)};` : "";
   const enc = !!input.authType && input.authType !== "none" ? `T:${input.authType};` : "";
   const hidden = input.hidden ? "H:true;" : "";
   return "WIFI:" + ssid + password + enc + hidden;
+};
+
+export interface WifiQrCodeProps {
+  wifi: WifiDetails;
+}
+
+export const WifiQrCode = ({ wifi }: Readonly<WifiQrCodeProps>) => {
+  const qrCode = generateQrCode(wifi);
+
+  return (
+    <>
+      <QRCodeSVG code={qrCode} />
+      <pre>
+        ssid: {wifi.ssid}
+        password: {wifi.password}
+        authType: {wifi.authType || "none"}
+        hidden: {(wifi.hidden || false).toString()}
+      </pre>
+    </>
+  );
 };
