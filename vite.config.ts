947e8f7491cf90fe7587b94fa5475a97cde48ed2
Simon Potter <simon@sjp.co.nz>
2025-06-18 22:13:28 +1200

    Migrate biome config and fix linting errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
-// biome-ignore lint/style/noDefaultExport: required by vite
 export default defineConfig({
   plugins: [
     preact({
